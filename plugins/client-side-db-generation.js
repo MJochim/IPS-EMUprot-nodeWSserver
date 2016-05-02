ee2c19bc18047b3430bc96a7e55e4005625eec59
Markus Jochim <markusjochim@phonetik.uni-muenchen.de>
2016-05-02 15:15:49 +0200

    client-side-db-generation is no longer dummy

--- plugins/client-side-db-generation.js
+++ plugins/client-side-db-generation.js
@@ -1,59 +1,209 @@
 // (c) 2016 Markus Jochim <markusjochim@phonetik.uni-muenchen.de>
 
 /**
- * This is the first plugin for the IPS-EMUprot-nodeWSserver. It serves as
- * an example for plugin authors.
+ * @todo this plugin has been forked from the bas plugin. documentation has
+ * to be rewritten. This plugin allows clients to create new bundles and
+ * sessions, and to upload media files.
  *
- * Technically, a plugin is a node module that exports a number of functions.
- * The functions serve as handlers for specific EMUprot events and override the
- * server's default handlers.
+ * This plugin allows databases to be accessed read-only via an
+ * authorization token instead of a username and password. It was designed
+ * for BAS to offer simple preview browsing of speech databases.
  *
- * It is only possible to override the handlers for:
- *
- * * GETBUNDLELIST,
- * * GETBUNDLE, and
- * * SAVEBUNDLE.
- *
- * That is, when a client sends a message with its type set to one of those
- * just mentioned, the respective function is called and it is expected to
- * send a response to the client.
+ * The plugin does three things:
+ * - Override any protocol functions that offer writing capabilities to the
+ *   client.
+ * - Modify the database configuration before it is sent to the client to
+ *   disable the save button for bundles.
+ * - Implement the authorization token to access databases.
  *
  */
 
-/**
- * Some init code can be placed here, if at all necessary.
- */
-
-// ...
-
-
-/**
- * Export your functions here. The main object must be named
- * exports.pluginMessageHandlers. It can have one or more of the properties
- * GETBUNDLELIST, GETBUNDLE, and SAVEBUNDLE (it is *not* necessary to
- * override all of them - just do not include the properties then). The
- * value of each must be a function (or function pointer) that accepts two
- * parameters:
- *
- * * mJSO The client's message as a Javsascript object. See protocol
- *        specification for details on how these are structured.
- * * wsConnect An object representing the client's WebSocket connection.
- */
 exports.pluginMessageHandlers = {
-	GETBUNDLELIST: pluginHandlerGetBundleList,
-	GETBUNDLE: pluginHandlerGetBundle,
-	SAVEBUNDLE: pluginHandlerSaveBundle
+	GETGLOBALDBCONFIG: pluginHandlerGetGlobalDBConfig,
+	GETDOUSERMANAGEMENT: pluginHandlerGetDoUserManagement,
+	GETPROTOCOL: pluginHandlerGetProtocol,
+
+	SAVEBUNDLE: pluginHandlerSaveBundle,
+	LOGONUSER: emptySuccessResponse
+
+	// Not overridden:
+	// - GETBUNDLE
+	// - GETBUNDLELIST
+	// - DISCONNECTWARNING
 };
 
+var main = require.main.exports;
 
-function pluginHandlerGetBundleList (mJSO, wsConnect) {
+var path = require('path');
+var fs = require('fs');
+var Q = require('q');
 
+function emptySuccessResponse(mJSO, wsConnect) {
+	main.sendMessage(wsConnect, mJSO.callbackID, true);
 }
 
-function pluginHandlerGetBundle (mJSO, wsConnect) {
-
+function emptyErrorResponse(mJSO, wsConnect) {
+	main.sendMessage(wsConnect, mJSO.callbackID, false);
 }
 
-function pluginHandlerSaveBundle (mJSO, wsConnect) {
+function pluginHandlerSaveBundle(mJSO, wsConnect) {
+	main.log.info('Saving: ' + mJSO.data.annotation.name,
+		'; clientID:', wsConnect.connectionID,
+		'; clientIP:', wsConnect._socket.remoteAddress);
 
+	// User input is escaped via path.normalize(); this way, ".." cannot
+	// be used to break out of the directory set by cfg.path2emuDBs
+	var sessionPath = path.join(
+		wsConnect.path2db,
+		path.normalize(mJSO.data.session + '_ses')
+	);
+	var bundleName = path.normalize(mJSO.data.annotation.name);
+
+	writeBundleToDisk(sessionPath, bundleName, mJSO.data, wsConnect).then(
+		function () {
+			main.sendMessage(wsConnect, mJSO.callbackID, true);
+		},
+		function (reason) {
+			main.log.error('caught this in event handler', reason);
+			main.sendMessage(wsConnect, mJSO.callbackID, false, reason);
+		}
+	)
+}
+
+function writeBundleToDisk(sessionPath, bundleName, data, wsConnect) {
+	var deferred = Q.defer();
+	main.log.info('wbtd()');
+	for (i in data) {
+		main.log.info('Data has', i);
+	}
+
+	// save annotation
+	var bundlePath = path.join(
+		sessionPath,
+		bundleName + '_bndl'
+	);
+	var annotJSONpath = path.join(
+		bundlePath,
+		path.normalize(bundleName + '_annot.json')
+	);
+	var mediaFilePath = path.join(
+		sessionPath,
+		bundleName + '_bndl',
+		path.normalize(bundleName + '.' + wsConnect.dbConfig.mediafileExtension)
+	);
+
+	try {
+		fs.mkdirSync(sessionPath);
+	} catch (error) {
+		if (error.code !== "EEXIST") {
+			deferred.reject(error);
+			return deferred.promise;
+		}
+	}
+
+	try {
+		fs.mkdirSync(bundlePath);
+	} catch (error) {
+		if (error.code !== "EEXIST") {
+			deferred.reject(error);
+			return deferred.promise;
+		}
+	}
+
+	fs.writeFile(annotJSONpath, JSON.stringify(data.annotation, undefined, 2), function (err) {
+		if (err) {
+			deferred.reject('Error writing annotation ' + err);
+		} else {
+			try {
+				main.log.info('in try catch now');
+				if (data.mediaFile) {
+					main.log.info('at mediaFile stuff');
+					var wave = new Buffer(data.mediaFile.data, 'base64');
+					fs.writeFileSync(mediaFilePath, wave);
+				}
+				main.log.info('past mediaFile stuff');
+
+				deferred.resolve();
+
+				if (main.cfg.use_git_if_repo_found) {
+					main.commitToGitRepo(wsConnect.path2db, wsConnect.ID, data.annotation.name, wsConnect.connectionID, wsConnect._socket.remoteAddress)
+						.then(function (value) {
+							deferred.resolve();
+						})
+						.catch(function (reason) {
+							deferred.reject(reason);
+						});
+				} else {
+					deferred.resolve();
+				}
+			} catch (error) {
+				main.log.error('caught this', error);
+				deferred.reject(error);
+			}
+		}
+	});
+
+	return deferred.promise;
+}
+
+function pluginHandlerGetProtocol(mJSO, wsConnect) {
+	// @todo GETPROTOCOL just isnt the right place for this. we should have an onConnection hook
+	var authToken = wsConnect.urlQuery.authToken;
+	if (authToken === undefined) {
+		authToken = '';
+	}
+	var bundleListPath = path.join(
+		wsConnect.path2db,
+		path.normalize(authToken + '_bundleList.json')
+	);
+
+	fs.readFile(bundleListPath, 'utf8', function (err, data) {
+		if (err) {
+			main.sendMessage(wsConnect, mJSO.callbackID, false, 'Invalid auth' +
+				' token');
+		} else {
+
+			main.log.info('Accepting auth token:', authToken, 'in:', wsConnect.path2db,
+				'; clientID:', wsConnect.connectionID,
+				'; clientIP:', wsConnect._socket.remoteAddress);
+
+			try {
+				// safely parse data:
+				// @todo any difference between this and jsonlint.parse() ?
+				var parsedData = JSON.parse(data);
+				wsConnect.bndlList = parsedData;
+				wsConnect.bndlListPath = bundleListPath;
+				wsConnect.authorised = true;
+
+				// Call original event handler
+				main.defaultHandlerGetProtocol(mJSO, wsConnect);
+			} catch (error) {
+				main.sendMessage(wsConnect, mJSO.callbackID, false, 'Error' +
+					' parsing _bundleList.json: ' + error);
+				main.log.info('caught exception in plugin');
+			}
+		}
+	});
+}
+
+function pluginHandlerGetDoUserManagement(mJSO, wsConnect) {
+	main.sendMessage(wsConnect, mJSO.callbackID, true, '', 'NO');
+}
+
+
+function pluginHandlerGetGlobalDBConfig(mJSO, wsConnect) {
+	var dbConfigPath = path.normalize(path.join(wsConnect.path2db, wsConnect.dbName + '_DBconfig.json'));
+
+	main.readGlobalDBConfigFromDisk(dbConfigPath, wsConnect).then(
+		function (value) {
+			// Disable save buttons for bundles
+			wsConnect.dbConfig.EMUwebAppConfig.activeButtons.saveBundle = false;
+
+			// Send configuration object to client
+			main.sendMessage(wsConnect, mJSO.callbackID, true, '', wsConnect.dbConfig);
+		},
+		function (reason) {
+			main.sendMessage(wsConnect, mJSO.callbackID, false, reason);
+		}
+	);
 }
