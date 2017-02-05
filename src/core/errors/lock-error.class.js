5d8d62735087625386a8046489c2cd9c5403653e
Markus Jochim <markusjochim@phonetik.uni-muenchen.de>
2017-02-05 20:05:48 +0100

    replaced [un]lockResource with [un]lock{Project|Database}

---
+++ src/core/errors/lock-error.class.js
@@ -0,0 +1,9 @@
+"use strict";
+
+const EmuError = require('./emu-error.class.js').EmuError;
+
+exports.LockError = class extends EmuError {
+	constructor () {
+		super ('E_LOCK', true, true);
+	}
+};
--- src/core/lock.js
+++ src/core/lock.js
@@ -1,35 +1,112 @@
 "use strict";
 
-exports.lockResource = lockResource;
-exports.unlockResource = unlockResource;
-exports.getLockInfo = getLockInfo;
+const LockError = require('./errors/lock-error.class').LockError;
 
-var lockID = 0;
-var lockedResources = {};
+let lockID = 0;
 
-function lockResource (resourceID) {
-	if (lockedResources.hasOwnProperty(resourceID)) {
-		return false;
-	} else {
-		++lockID;
-		lockedResources[resourceID] = lockID;
-		return lockID;
+// Lock individual databases
+let lockedDatabases = {};
+
+// Lock entire projects
+let lockedProjects = {};
+
+exports.lockDatabase = function (project, database, retries = 2, retryInterval = 1000) {
+	if (typeof project !== 'string' || typeof database !== 'string') {
+		return Promise.reject();
 	}
+
+	return new Promise((resolve, reject) => {
+		if (
+			lockedProjects.hasOwnProperty(project) ||
+			(lockedDatabases.hasOwnProperty(project) && lockedDatabases[project].hasOwnProperty(database))
+		) {
+			if (retries > 0) {
+				setTimeout(() => {
+					resolve(exports.lockDatabase(project, database, retries - 1, retryInterval));
+				}, retryInterval);
+			} else {
+				reject(new LockError());
+			}
+		} else {
+			++lockID;
+			if (!lockedDatabases[project]) {
+				lockedDatabases[project] = {};
+			}
+			lockedDatabases[project][database] = lockID;
+			resolve(lockID);
+		}
+	});
+};
+
+exports.unlockDatabase = function (project, database, lockID) {
+	if (typeof project !== 'string' || typeof database !== 'string' || typeof lockID !== 'number') {
+		return Promise.reject();
+	}
+
+	return new Promise((resolve, reject) => {
+		if (
+			lockedDatabases.hasOwnProperty(project) &&
+			lockedDatabases[project].hasOwnProperty(database) &&
+			lockedDatabases[project][database] === lockID
+		) {
+			delete lockedDatabases[project][database];
+			if (Object.keys(lockedDatabases[project]).length === 0) {
+				delete lockedDatabases[project];
+			}
+			resolve();
+		} else {
+			reject();
+		}
+	});
 }
 
-function unlockResource (resourceID) {
-	if (lockedResources.hasOwnProperty(resourceID)) {
-		delete lockedResources[resourceID];
-	} else {
-		return false;
+exports.lockProject = function (project, retries = 2, retryInterval = 1000) {
+	if (typeof project !== 'string') {
+		return Promise.reject();
 	}
+
+	return new Promise((resolve, reject) => {
+		if (
+			lockedProjects.hasOwnProperty(project) ||
+			lockedDatabases.hasOwnProperty(project)
+		) {
+			if (retries > 0) {
+				setTimeout(() => {
+					resolve(exports.lockProject(project, retries - 1, retryInterval));
+				}, retryInterval);
+			} else {
+				reject(new LockError());
+			}
+		} else {
+			++lockID;
+			lockedProjects[project] = lockID;
+			resolve(lockID);
+		}
+	});
+};
+
+exports.unlockProject = function (project, lockID) {
+	if (typeof project !== 'string' || typeof lockID !== 'number') {
+		return Promise.reject();
+	}
+
+	return new Promise((resolve, reject) => {
+		if (
+			lockedProjects.hasOwnProperty(project) &&
+			lockedProjects[project] === lockID
+		) {
+			delete lockedProjects[project];
+			resolve();
+		} else {
+			reject();
+		}
+	});
 }
 
-function getLockInfo () {
-	var result = {};
-	for (i in lockedResources) {
-		result[i] = lockedResources[i];
-	}
-	return result;
-}
-
+exports.getLockInfo = function () {
+	// @todo this should really make a deep copy
+	return {
+		projects: lockedProjects,
+		databases: lockedDatabases
+	};
+};
