d7d33696f557275767b577d99fb81b749f316dee
Markus Jochim <markusjochim@phonetik.uni-muenchen.de>
2017-02-05 20:23:35 +0100

    made listCommits() use the locking mechanism

--- src/manager-api/query-handlers/list-commits.function.js
+++ src/manager-api/query-handlers/list-commits.function.js
@@ -4,51 +4,62 @@ const nodegit = require('nodegit');
 
 const FilenameHelper = require('../../core/filename-helper.class').FilenameHelper;
 const GitCommit = require('../../core/types/git-commit.class').GitCommit;
+const lock = require('../../core/lock');
 
 
 exports.listCommits = function (project, database) {
 	return new Promise((resolve, reject) => {
-		let databasePath = FilenameHelper.databaseDirectory(project, database);
-		let result = [];
+		lock.lockDatabase(project, database)
+			.then((lockID) => {
+				let databasePath = FilenameHelper.databaseDirectory(project, database);
 
-		// Open the repository directory
-		nodegit.Repository.open(databasePath)
-			.then(function (repo) {
-				// Open the master branch.
-				return repo.getMasterCommit();
-			})
+				// Open the repository directory
+				nodegit.Repository.open(databasePath)
+					.then(function (repo) {
+						// Open the master branch.
+						return repo.getMasterCommit();
+					})
 
-			// Display information about commits on master.
-			.then(function (firstCommitOnMaster) {
-				// Create a new history event emitter.
-				let history = firstCommitOnMaster.history();
+					// Display information about commits on master.
+					.then(function (firstCommitOnMaster) {
+						// Create a new history event emitter.
+						let history = firstCommitOnMaster.history();
 
-				history.on('error', (error) => {
-					reject(error);
-				});
-
-				// The onEnd event will fire only once. It will fire as soon
-				// as all commits are known.
-				history.on('end', (commits) => {
-					try {
-						commits.forEach((commit) => {
-							let currentCommit = new GitCommit();
-
-							currentCommit.commitID = commit.sha();
-							currentCommit.date = commit.date();
-							currentCommit.message = commit.message();
-
-							result.push(currentCommit);
+						history.on('error', (error) => {
+							lock.unlockDatabase(project, database, lockID);
+							reject(error);
 						});
 
-						resolve(result);
-					} catch (error) {
-						reject(error);
-					}
-				});
+						// The onEnd event will fire only once. It will fire as soon
+						// as all commits are known.
+						history.on('end', (commits) => {
+							try {
+								let result = [];
+								commits.forEach((commit) => {
+									let currentCommit = new GitCommit();
 
-				// Start emitting events
-				history.start();
+									currentCommit.commitID = commit.sha();
+									currentCommit.date = commit.date();
+									currentCommit.message = commit.message();
+
+									result.push(currentCommit);
+								});
+
+								lock.unlockDatabase(project, database, lockID);
+								resolve(result);
+							} catch (error) {
+								lock.unlockDatabase(project, database, lockID);
+								reject(error);
+							}
+						});
+
+						// Start emitting events
+						history.start();
+					})
+					.catch((error) => {
+						lock.unlockDatabase(project, database, lockID);
+						reject(error);
+					});
 			})
 			.catch((error) => {
 				reject(error);
