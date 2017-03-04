6692d332d1b9b946ad9108e6b6f7b8e25304fb74
Markus Jochim <markusjochim@phonetik.uni-muenchen.de>
2017-03-04 04:26:34 +0100

    FilenameHelper can now return relative paths

--- src/core/filename-helper.class.js
+++ src/core/filename-helper.class.js
@@ -8,30 +8,34 @@ exports.FilenameHelper = class FilenameHelper {
 	//
 	// Paths that belong to the whole project
 	//
-	static projectDirectory(project) {
-		return path.join(
-			config.dataDirectory,
-			project
-		);
+	static projectDirectory(project, absolute = true) {
+		if (absolute) {
+			return path.join(
+				config.dataDirectory,
+				project
+			);
+		} else {
+			return '';
+		}
 	}
 
-	static projectDatabasesDirectory (project) {
+	static projectDatabasesDirectory(project, absolute = true) {
 		return path.join(
-			FilenameHelper.projectDirectory(project),
+			FilenameHelper.projectDirectory(project, absolute),
 			'databases'
 		);
 	}
 
-	static projectUploadsDirectory (project) {
+	static projectUploadsDirectory(project, absolute = true) {
 		return path.join(
-			FilenameHelper.projectDirectory(project),
+			FilenameHelper.projectDirectory(project, absolute),
 			'uploads'
 		);
 	}
 
-	static projectDownloadsDirectory (project) {
+	static projectDownloadsDirectory(project, absolute = true) {
 		return path.join(
-			FilenameHelper.projectDirectory(project),
+			FilenameHelper.projectDirectory(project, absolute),
 			'downloads'
 		);
 	}
@@ -39,55 +43,59 @@ exports.FilenameHelper = class FilenameHelper {
 	//
 	// Paths that belong to a given database
 	//
-	static databaseDirectory (project, database) {
-		return path.join(
-			FilenameHelper.projectDatabasesDirectory(project),
-			database + '_emuDB'
-		);
+	static databaseDirectory(project, database, absolute = true) {
+		if (absolute) {
+			return path.join(
+				FilenameHelper.projectDatabasesDirectory(project),
+				database + '_emuDB'
+			);
+		} else {
+			return '';
+		}
 	}
 
-	static databaseConfigFile (project, database) {
+	static databaseConfigFile(project, database, absolute = true) {
 		return path.join(
-			FilenameHelper.databaseDirectory (project, database),
+			FilenameHelper.databaseDirectory(project, database, absolute),
 			database + '_DBconfig.json'
 		);
 	}
 
-	static databaseBundleListsDirectory (project, database) {
+	static databaseBundleListsDirectory(project, database, absolute = true) {
 		return path.join(
-			FilenameHelper.databaseDirectory(project, database),
+			FilenameHelper.databaseDirectory(project, database, absolute),
 			'bundleLists'
 		);
 	}
 
-	static databaseArchiveLabelDirectory (project, database, archiveLabel) {
+	static databaseArchiveLabelDirectory(project, database, archiveLabel, absolute = true) {
 		if (archiveLabel) {
 			return path.join(
-				FilenameHelper.databaseBundleListsDirectory(project, database),
+				FilenameHelper.databaseBundleListsDirectory(project, database, absolute),
 				archiveLabel + '_archiveLabel'
 			);
 		} else {
-			return FilenameHelper.databaseBundleListsDirectory(project, database);
+			return FilenameHelper.databaseBundleListsDirectory(project, database, absolute);
 		}
 	}
 
-	static databaseBundleListFile (project, database, archiveLabel, bundleList) {
+	static databaseBundleListFile(project, database, archiveLabel, bundleList, absolute = true) {
 		return path.join(
-			FilenameHelper.databaseArchiveLabelDirectory(project, database, archiveLabel),
+			FilenameHelper.databaseArchiveLabelDirectory(project, database, archiveLabel, absolute),
 			bundleList + '_bundleList.json'
 		)
 	}
 
-	static databaseSessionDirectory (project, database, session) {
+	static databaseSessionDirectory(project, database, session, absolute = true) {
 		return path.join(
-			FilenameHelper.databaseDirectory (project, database),
+			FilenameHelper.databaseDirectory(project, database, absolute),
 			session + '_ses'
 		);
 	}
 
-	static databaseBundleDirectory (project, database, session, bundle) {
+	static databaseBundleDirectory(project, database, session, bundle, absolute = true) {
 		return path.join(
-			FilenameHelper.databaseSessionDirectory (project, database, session),
+			FilenameHelper.databaseSessionDirectory(project, database, session, absolute),
 			bundle + '_bndl'
 		);
 	}
@@ -95,23 +103,27 @@ exports.FilenameHelper = class FilenameHelper {
 	//
 	// Paths that belong to a given upload
 	//
-	static uploadDirectory (project, upload) {
-		return path.join(
-			FilenameHelper.projectUploadsDirectory(project),
-			upload
-		);
+	static uploadDirectory(project, upload, absolute = true) {
+		if (absolute) {
+			return path.join(
+				FilenameHelper.projectUploadsDirectory(project),
+				upload
+			);
+		} else {
+			return '';
+		}
 	}
 
-	static uploadDataDirectory (project, upload) {
+	static uploadDataDirectory(project, upload, absolute = true) {
 		return path.join(
-			FilenameHelper.uploadDirectory(project, upload),
+			FilenameHelper.uploadDirectory(project, upload, absolute),
 			'data'
 		);
 	}
 
-	static uploadDatabaseDirectory (project, upload, database) {
+	static uploadDatabaseDirectory(project, upload, database, absolute = true) {
 		return path.join(
-			FilenameHelper.uploadDataDirectory(project, upload),
+			FilenameHelper.uploadDataDirectory(project, upload, absolute),
 			database + '_emuDB'
 		)
 	}
